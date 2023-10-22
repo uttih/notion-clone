b7ae0c25a34c2d587e2e67d8f68427105fc5c573
wkjy <u.hikaru0830@gmail.com>
2023-10-22 16:30:53 +0900

    2023/10/22
    3-13

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+//DB接続
+try {
+  mongoose.connect(
+    "mongodb+srv://uttih:hikaru1247@cluster0.knthgwm.mongodb.net/"
+  );
+  console.log("DBと接続中・・・");
+} catch (error) {
+  console.log(error);
+}
 
 //ユーザー新規登録API
 
