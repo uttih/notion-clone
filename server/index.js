f1acc3f995a4c0574e08086de39bb0fad5af190d
wkjy <u.hikaru0830@gmail.com>
2023-10-22 15:36:01 +0900

    2023/10/22

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
 app.listen(PORT, () => {
   console.log("ローカルサーバ起動中・・・");
 });
