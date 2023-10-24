f20a6bf11e1d24146e314c87b53762349ad47afa
wkjy <u.hikaru0830@gmail.com>
2023-10-24 21:13:19 +0900

    2023/10/24
    4-22

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const CryptoJS = require("crypto-js");
 const JWT = require("jsonwebtoken");
+const { body } = require("express-validator");
 const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5000;
@@ -18,24 +19,39 @@ try {
 }
 
 //ユーザー新規登録API
-app.post("/register", async (req, res) => {
-  //パスワードの受けとり
-  const password = req.body.password;
+app.post(
+  "/register",
+  body("username")
+    .isLength({ min: 8 })
+    .withMessage("ユーザー名は8文字以上である必要があります"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("パスワードは8文字以上である必要があります"),
+  body("confirmpassword")
+    .isLength({ min: 8 })
+    .withMessage("確認用パスワードは8文字以上である必要があります"),
+  async (req, res) => {
+    //パスワードの受けとり
+    const password = req.body.password;
 
-  try {
-    //パスワードの暗号化
-    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
-    //ユーザーの新規作成
-    const user = await User.create(req.body);
-    //JWTの発行
-    const token = JWT.sigh({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-      expiresIn: "24h",
-    });
-    return res.status(200).json({ user, token });
-  } catch (err) {
-    return res.status(500).json(err);
+    try {
+      //パスワードの暗号化
+      req.body.password = CryptoJS.AES.encrypt(
+        password,
+        process.env.SECRET_KEY
+      );
+      //ユーザーの新規作成
+      const user = await User.create(req.body);
+      //JWTの発行
+      const token = JWT.sigh({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+        expiresIn: "24h",
+      });
+      return res.status(200).json({ user, token });
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   }
-});
+);
 
 //ユーザーログイン用APi
 
