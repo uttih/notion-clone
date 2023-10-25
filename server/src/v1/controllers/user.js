693f75680b251689ba1e2092f29ec5f9b472a917
wkjy <u.hikaru0830@gmail.com>
2023-10-25 21:29:53 +0900

    2023/10/25
    4-27

---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,24 @@
+const JWT = require("jsonwebtoken");
+const CryptoJS = require("crypto-js");
+const User = require("../models/user");
+
+exports.register = async (req, res) => {
+  //パスワードの受けとり
+  const password = req.body.password;
+
+  try {
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成
+    const user = await User.create(req.body);
+    //JWTの発行
+    const token = JWT.sigh({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+//ユーザーログイン用APi
