d62123c9418eb76902014f0438106c94b5af9cd8
wkjy <u.hikaru0830@gmail.com>
2023-10-27 21:05:23 +0900

    2023/10/27
    4-32

---
+++ server/src/v1/handlers/tokenHandler.js
@@ -0,0 +1,4 @@
+//クライアントから渡されたJWTが正常か検証
+
+//JWT認証を検証するためのミドルウェア
+exports.verifyToken = (req, res, next) => {};
--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -43,5 +43,8 @@ router.post(
 );
 
 //JWT認証API
+router.post("/verify-token", (req, res) => {
+  return res.status(200).json({ user: req.user });
+});
 
 module.exports = router;
