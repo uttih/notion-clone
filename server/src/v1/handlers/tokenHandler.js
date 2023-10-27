6d6b53662b0e28c10855b6e1dca5f422925783fa
wkjy <u.hikaru0830@gmail.com>
2023-10-27 21:12:15 +0900

    2023/10/27
    4-33

--- server/src/v1/handlers/tokenHandler.js
+++ server/src/v1/handlers/tokenHandler.js
@@ -1,4 +1,20 @@
+const JWT = require("jsonwebtoken");
+
 //クライアントから渡されたJWTが正常か検証
+const tokenDecode = (req) => {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ")[1];
+    try {
+      const decodedToken = JWT.verify(bearer, process.env.TOKEN_SECRET_KEY);
+      return decodedToken;
+    } catch {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
 
 //JWT認証を検証するためのミドルウェア
 exports.verifyToken = (req, res, next) => {};
