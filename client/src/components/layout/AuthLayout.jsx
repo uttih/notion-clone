cc709ea69252e6464e1f005bedd5d1be6ea836d2
wkjy <u.hikaru0830@gmail.com>
2023-10-28 19:09:11 +0900

    2023/10/28
    5-42

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,10 +1,28 @@
+import { Box, Container } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import notionLogo from "../../assets/images/notion-logo.png";
 
 const AuthLayout = () => {
   return (
     <div>
-      AuthLayout
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={notionLogo}
+            alt=""
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notion クローン開発
+        </Box>
+      </Container>
       <Outlet />
     </div>
   );
