f9e370ac475ca0e57d01bf27368dfbe7bb7d7e10
wkjy <u.hikaru0830@gmail.com>
2023-10-28 16:12:26 +0900

    2023/10/28
    5-41

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 
 const Login = () => {
-  return (
-    <div>
-      Login <AccessAlarmIcon />
-    </div>
-  );
+  return <div>Login</div>;
 };
 
 export default Login;
