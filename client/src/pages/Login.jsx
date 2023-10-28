1c54403c5017b9fa8f25c3037b463ebb523741cc
wkjy <u.hikaru0830@gmail.com>
2023-10-28 16:05:57 +0900

    2023/10/28
    5-40
    MUI install

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login <AccessAlarmIcon />
+    </div>
+  );
 };
 
 export default Login;
