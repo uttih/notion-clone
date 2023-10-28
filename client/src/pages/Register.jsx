f2ca85ed0f19e555064bb5fc4c5bbe07186e04ff
wkjy <u.hikaru0830@gmail.com>
2023-10-29 01:59:37 +0900

    2023/10/29
    5-44
    import {LoadingButton}

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,36 +1,53 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import React from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <Box component="form">
-      <TextField
-        fullWidth
-        id="username"
-        label="お名前"
-        margin="normal"
-        name="username"
-        required
-      />
-      <TextField
-        fullWidth
-        id="password"
-        label="パスワード"
-        margin="normal"
-        name="password"
-        type="password"
-        required
-      />
-      <TextField
-        fullWidth
-        id="confirmPassword"
-        label="確認用パスワード"
-        margin="normal"
-        name="confirmPassword"
-        type="password"
-        required
-      />
-    </Box>
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          id="username"
+          label="お名前"
+          margin="normal"
+          name="username"
+          required
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="パスワード"
+          margin="normal"
+          name="password"
+          type="password"
+          required
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          label="確認用パスワード"
+          margin="normal"
+          name="confirmPassword"
+          type="password"
+          required
+        />
+        <LoadingButton
+          sx={{ mt: 3, bm: 2 }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="outlined"
+        >
+          アカウント作成
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">
+        すでにアカウントを持っていますか？ログイン
+      </Button>
+    </>
   );
 };
 
