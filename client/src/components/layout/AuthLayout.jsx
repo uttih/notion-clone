42bcfcb27590932ab7f6f329f3879538171d2a3e
wkjy <u.hikaru0830@gmail.com>
2023-10-31 21:52:08 +0900

    2023/10/31
    5-61

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,9 +1,21 @@
 import { Box, Container } from "@mui/material";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import notionLogo from "../../assets/images/notion-logo.png";
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect =
+    (() => {
+      //JWTを持っているのか確認する。
+      const checkAuth = async () => {
+        //認証チェック
+      };
+      checkAuth();
+    },
+    [navigate]);
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
