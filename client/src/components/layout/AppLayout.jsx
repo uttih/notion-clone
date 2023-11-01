f6efd33fb5553937dc967106fde535bb9c688736
wkjy <u.hikaru0830@gmail.com>
2023-11-01 16:52:20 +0900

    2023/11/01
    6-65

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Box, Container } from "@mui/material";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
+import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //JWTを持っているのか確認する。
+    const checkAuth = async () => {
+      //認証チェック
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default AppLayout;
