7f9accdc3b4ec9dd03053c2c7186d137d7b01953
wkjy <u.hikaru0830@gmail.com>
2023-11-05 16:13:46 +0900

    2023/11/5
    6-75
    schema

---
+++ server/src/v1/models/memo.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const memoSchema = new mongoose.Schema({
+  user: {
+    type: Schema.types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: "📝",
+  },
+  title: {
+    type: String,
+    default: "無題",
+  },
+  description: {
+    type: String,
+    default: "ここに自由に記入してください",
+  },
+  position: {
+    type: number,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  favoritePosition: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("Memo", memoSchema);
