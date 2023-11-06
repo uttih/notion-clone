0bc2c7798f05edc7f33fc8390e71c3fe9d1dd3bb
wkjy <u.hikaru0830@gmail.com>
2023-11-06 17:42:54 +0900

    2023/11/6
    6-77
    memo api

--- server/src/v1/controllers/memo.js
+++ server/src/v1/controllers/memo.js
@@ -1,9 +1,14 @@
-const memo = require("../models/memo");
+const Memo = require("../models/memo");
 
 exports.create = async (req, res) => {
   try {
+    const memoCount = await Memo.find().count();
     //メモ新規作成
-    const memo = await memo.create({});
+    const memo = await Memo.create({
+      user: req.user._id,
+      position: memoCount > 0 ? memoCount : 0,
+    });
+    res.status(201).json(memo);
   } catch {
     res.status(500).json(err);
   }
