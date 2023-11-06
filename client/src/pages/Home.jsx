9b14f14c6aeb1727e29558d77e8426e57cee9df8
wkjy <u.hikaru0830@gmail.com>
2023-11-06 17:57:25 +0900

    2023/11/6
    6-79

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+
+  const createMemo = () => {};
+
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <LoadingButton
+        variant="outlined"
+        color="success"
+        onClick={() => createMemo}
+        loading={loading}
+      >
+        最初のメモを作成
+      </LoadingButton>
+    </Box>
+  );
 };
 
 export default Home;
