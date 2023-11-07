f2e307b9ce56ddb3491db3b663ad3dea1c72e660
wkjy <u.hikaru0830@gmail.com>
2023-11-08 02:15:32 +0900

    2023/11/8
    7-86

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -8,16 +8,18 @@ import {
 } from "@mui/material";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import assets from "../../assets/index";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import memoApi from "../../api/memoApi";
 import { setMemo } from "../../redux/features/memoSlice";
 
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { memoId } = useParams();
   const user = useSelector((state) => state.user.value);
   const memos = useSelector((state) => state.memo.value);
 
@@ -39,6 +41,11 @@ const Sidebar = () => {
     };
     getMemos();
   }, [dispatch]);
+
+  useEffect(() => {
+    const activeIndex = memos.findIndex((e) => e._id === memoId);
+    setActiveIndex(activeIndex);
+  }, [navigate]);
 
   return (
     <Drawer
@@ -110,6 +117,7 @@ const Sidebar = () => {
             component={Link}
             to={`/memo/${item._id}`}
             key={item._id}
+            selected={index === activeIndex}
           >
             <Typography>
               {item.icon} {item.title}
