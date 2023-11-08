4ae483132b50c68705f67554bbe34513f2053926
wkjy <u.hikaru0830@gmail.com>
2023-11-08 18:23:50 +0900

    2023/11/8
    7-87

--- server/src/v1/controllers/memo.js
+++ server/src/v1/controllers/memo.js
@@ -21,3 +21,14 @@ exports.getAll = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+exports.getOne = async (req, res) => {
+  const { memoId } = req.params;
+  try {
+    const memo = Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res / status(404).json("メモが存在しません");
+    res.status(200).json(memo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- server/src/v1/routes/memo.js
+++ server/src/v1/routes/memo.js
@@ -8,4 +8,7 @@ router.post("/", tokenHandler.verifyToken, memoController.create);
 //ログインしているユーザーが投稿したメモをすべて取得
 router.get("/", tokenHandler.verifyToken, memoController.getAll);
 
+//ログインしているユーザーが投稿したメモを1つ取得
+router.get("/:memoId", tokenHandler.verifyToken, memoController.getOne);
+
 module.exports = router;
