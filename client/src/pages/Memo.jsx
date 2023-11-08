fc39ed9b5d75e5dcedf7024ec5536ed893321f3c
wkjy <u.hikaru0830@gmail.com>
2023-11-09 01:01:47 +0900

    2023/11/9
    7-88

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, IconButton, TextField } from "@mui/material";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutline";
 
 const Memo = () => {
-  return <div>Memo</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space=between",
+          width: "100%",
+        }}
+      >
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton variant="outlined" color="error">
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: "10px 50px" }}>
+        <TextField placeholder="無題" variant="outlined" fullWidth />
+        <TextField placeholder="追加" variant="outlined" fullWidth />
+      </Box>
+    </>
+  );
 };
 
 export default Memo;
