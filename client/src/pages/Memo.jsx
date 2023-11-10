720567a827bb5040ccf25698e199e69bbd88bf7a
wkjy <u.hikaru0830@gmail.com>
2023-11-10 17:55:03 +0900

    2023/11/10
    7-96

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Box, IconButton, TextField } from "@mui/material";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutline";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import memoApi from "../api/memoApi";
+import { useDispatch, useSelector } from "react-redux";
+import { setMemo } from "../redux/features/memoSlice";
 
 const Memo = () => {
   const { memoId } = useParams();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const memos = useSelector((state) => state.memo.value);
 
   useEffect(() => {
     const getMemo = async () => {
@@ -59,6 +64,14 @@ const Memo = () => {
     try {
       const deletedMemo = await memoApi.delete(memoId);
       console.log(deletedMemo);
+      const newMemos = memos.filter((e) => e._id !== memoId);
+      if (newMemos.length === 0) {
+        navigate("/memo");
+      } else {
+        navigate(`/memo/${newMemos[0]._id}`);
+      }
+
+      dispatch(setMemo(newMemos));
     } catch (err) {
       alert(err);
     }
